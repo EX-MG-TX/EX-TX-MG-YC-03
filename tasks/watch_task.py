04f125b829d811172ee5c77f89e8f290c9f99b41
EX-MG-TX <80203485+EX-MG-TX@users.noreply.github.com>
2021-03-10 18:13:07 +0800

    Update watch_task.py

--- tasks/watch_task.py
+++ tasks/watch_task.py
@@ -5,6 +5,7 @@ import logging
 import random
 
 async def watch_task(biliapi: asyncbili) -> None:
+	nub = random.randint(0,3);
     try:
         ret = await get_ids(biliapi)
     except Exception as e:
@@ -17,9 +18,9 @@ async def watch_task(biliapi: asyncbili) -> None:
         webhook.addMsg('msg_simple', f'{biliapi.name}:模拟视频观看失败\n')
         return
     ids = [{"aid":"204516234","cid":"307178951"},{"aid":"756904275","cid":"304166415"},{"aid":"204502833","cid":"307178835"},{"aid":"459368214","cid":"299736533"}]
+			
     try:
-		nub = random.randint(0,3);
-		ret = await biliapi.report(ids[nub]["aid"], ids[nub]["cid"], 300)
+	ret = await biliapi.report(ids[nub]["aid"], ids[nub]["cid"], 300)
         if ret["code"] == 0:
             logging.info(f'{biliapi.name}: 成功模拟观看av号为{ids[nub]["aid"]}的视频')
         else:
